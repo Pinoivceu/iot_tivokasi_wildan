cbf8455560a5292018ccff1552228b4c53c370d2
pinoivceu <apipkph@gmail.com>
2025-03-06 13:28:30 +0700

    Minggu 2

---
+++ simulasi-relay-button-led/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USB.cpp.o: \
+ /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
+ /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
