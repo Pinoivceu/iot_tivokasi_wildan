965b4db7fa243aa2006ac5141c10f0e418d68e2d
pinoivceu <apipkph@gmail.com>
2025-03-06 14:19:47 +0700

    delete

--- simulasi-relay-button-led/.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/pinoivceu/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
